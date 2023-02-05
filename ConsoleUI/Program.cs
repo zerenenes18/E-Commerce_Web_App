a49493adbc2399821c91c6f866d1938f5370c780
scout0118 <104466426+scout0118@users.noreply.github.com>
2023-02-05 14:22:29 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var p in productManager.GetByUnitPrice(10,20))
+            {
+                Console.WriteLine("Product Name :" + p.ProductName 
+                                + " ---CategoryID:" + p.CategoryId 
+                                + " ---Price:" + p.UnitPrice );
+            } 
+        }
+    }
+}
